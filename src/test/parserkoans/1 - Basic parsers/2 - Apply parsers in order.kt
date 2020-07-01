67243a3ae665f421f6f228f37ad4877e3d3b5b74
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-07-01 18:33:12 +0100

    use Pair and Triple rather than List2, List3

--- src/test/parserkoans/1 - Basic parsers/2 - Apply parsers in order.kt
+++ src/test/parserkoans/1 - Basic parsers/2 - Apply parsers in order.kt
@@ -15,15 +15,15 @@ import org.junit.Test
  */
 
 fun <T1, T2> inOrder(parser1: Parser<T1>, parser2: Parser<T2>) =
-    object : Parser<List2<T1, T2>> {
-        override fun parse(input: Input): Output<List2<T1, T2>>? {
+    object : Parser<Pair<T1, T2>> {
+        override fun parse(input: Input): Output<Pair<T1, T2>>? {
             TODO()
         }
     }
 
 fun <T1, T2, T3> inOrder(parser1: Parser<T1>, parser2: Parser<T2>, parser3: Parser<T3>) =
-    object : Parser<List3<T1, T2, T3>> {
-        override fun parse(input: Input): Output<List3<T1, T2, T3>>? {
+    object : Parser<Triple<T1, T2, T3>> {
+        override fun parse(input: Input): Output<Triple<T1, T2, T3>>? {
             TODO()
         }
     }
@@ -40,7 +40,7 @@ class `Step 2 - apply parsers in order` {
         val parser = inOrder(string("foo"), string("bar"))
         val input = Input("foobar")
         parser.parse(input) shouldEqual Output(
-            payload = List2("foo", "bar"),
+            payload = Pair("foo", "bar"),
             nextInput = input.consumed()
         )
     }
@@ -49,7 +49,7 @@ class `Step 2 - apply parsers in order` {
         val parser = inOrder(string("foo"), string("bar"), string("buz"))
         val input = Input("foobarbuz")
         parser.parse(input) shouldEqual Output(
-            payload = List3("foo", "bar", "buz"),
+            payload = Triple("foo", "bar", "buz"),
             nextInput = input.consumed()
         )
     }
--- src/test/parserkoans/util/Lists.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("PackageDirectoryMismatch", "unused")
-
-package parserkoans
-
-data class List1<T1>(val value1: T1)
-data class List2<T1, T2>(val value1: T1, val value2: T2)
-data class List3<T1, T2, T3>(val value1: T1, val value2: T2, val value3: T3)
-data class List4<T1, T2, T3, T4>(val value1: T1, val value2: T2, val value3: T3, val value4: T4)
